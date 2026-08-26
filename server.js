860e1d409842bc102e0f4bff4feecf23dd683acc
Jmark <pinedajaymark99@gmial.com>
2026-08-26 22:24:35 +0800

    all goods 8/24/26

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // Serve frontend files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
